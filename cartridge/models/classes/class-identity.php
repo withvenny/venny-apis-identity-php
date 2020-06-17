1ef6188e7b00a0ac217fbbd629f4994d7ab085c3
adolphusnolan <adolphusnolan@gmail.com>
2020-06-17 18:48:39 -0500

    krakow

--- cartridge/models/classes/class-identity.php
+++ cartridge/models/classes/class-identity.php
@@ -1281,7 +1281,23 @@
                 
                     //
                     while($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
-        
+
+                        //echo var_dump($data);
+
+                        //exit;
+
+                        // User information...
+                        $request['id'] = $row['user_id']; // get user ID for selectUsers
+                        $request['domain'] = 'users'; // switch domain
+                        $user_details = $this->user->selectUsers($request); // collect all user details returned from selectUsers
+                        $data[]['alias'] = $user_details['data'][0]['alias']; // add alias to $data
+
+                        $request['id'] = $user_details['data'][0]['person']; // get user ID for selectUsers
+                        $request['domain'] = 'persons'; // switch domain
+                        $person_details = $this->person->selectPersons($request); // collect all user details returned from selectUsers
+                        $data[]['name_first'] = $person_details['data'][0]['name_first']; // add first name to $data
+                        $data[]['name_last'] = $person_details['data'][0]['name_last']; // add last name to $data
+
                         //
                         $data[] = [
 
@@ -1295,25 +1311,10 @@
                             'user' => $row['user_id'],
                             'app' => $row['app_id'],
                             'when' => $row['time_finished'],
-
-                                                    //echo var_dump($data);
-
-                        //exit;
-
-                        // User information...
-                        $request['id'] = $data[0]['user']; // get user ID for selectUsers
-                        $request['domain'] = 'users'; // switch domain
-                        $user_details = $this->user->selectUsers($request); // collect all user details returned from selectUsers
-                        $data['alias'] = $user_details['data'][0]['alias']; // add alias to $data
-                        $yo = $user_details['data'][0]['person']; // add person to $data
-
-                        // Person information...
-                        $request['id'] = $user_details['data'][0]['person']; // get user ID for selectUsers
-                        $request['domain'] = 'persons'; // switch domain
-                        $person_details = $this->person->selectPersons($request); // collect all user details returned from selectUsers
-                        $data[]['name_first'] = $person_details['data'][0]['name_first']; // add first name to $data
-                        $data[]['name_last'] = $person_details['data'][0]['name_last']; // add last name to $data
-
+                            'name_first' => $row['time_finished'],
+                            'name_last' => $row['time_finished'],
+                            'alias' => $row['time_finished'],
+                            
 
                         ];
 
