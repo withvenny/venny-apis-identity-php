bf6463fb2581ccfb8553a96e7ad22c0f73f6fb5f
Al <adolphusnolan@gmail.com>
2022-03-10 11:30:12 -0600

    person column now populating

--- cartridge/models/classes/class-identity.php
+++ cartridge/models/classes/class-identity.php
@@ -658,6 +658,7 @@
             if(isset($request['status'])){$columns.="user_status,";}
             if(isset($request['validation'])){$columns.="user_validation,";}
             if(isset($request['welcome'])){$columns.="user_welcome,";}
+            if(isset($request['person'])){$columns.="person_id,";}
             $columns.= "app_id,";
             $columns.= "event_id,";
             $columns.= "process_id";
@@ -673,6 +674,7 @@
             if(isset($request['status'])){$values.=":user_status,";}
             if(isset($request['validation'])){$values.=":user_validation,";}
             if(isset($request['welcome'])){$values.=":user_welcome,";}
+            if(isset($request['person'])){$values.=":person_id,";}
 
             $values.= ":app_id,";
             $values.= ":event_id,";
@@ -702,6 +704,7 @@
             if(isset($request['status'])){$statement->bindValue('user_status',$request['status']);}
             if(isset($request['validation'])){$statement->bindValue('user_validation',$request['validation']);}
             if(isset($request['welcome'])){$statement->bindValue('user_welcome',$request['welcome']);}      	
+            if(isset($request['person'])){$statement->bindValue('person_id',$request['person']);}      	
             $statement->bindValue(':app_id', $request['app']);
             $statement->bindValue(':event_id', $this->token->event_id());
             $statement->bindValue(':process_id', $this->token->process_id());
