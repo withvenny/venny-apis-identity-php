10ee2fbfea4e887c3f01502b842dc8a6523d926d
Al <adolphusnolan@gmail.com>
2022-03-13 10:24:38 -0500

    added person to call

--- cartridge/models/classes/class-identity.php
+++ cartridge/models/classes/class-identity.php
@@ -905,8 +905,8 @@
                             'status' => $row['user_status'],
                             'validation' => $row['user_validation'],
                             'welcome' => json_decode($row['user_welcome']),
-                            'app' => $row['app_id'],
                             'person' => $row['person_id'],
+                            'app' => $row['app_id'],
                             'when' => $row['time_finished'],
 
                         ];
